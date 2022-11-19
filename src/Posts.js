97da3ffd843fc09d5f136d103ecc38c6fc2f60b4
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-11-19 00:54:03 -0300

    Salvando posts, dando like e aumentando nmr de likes.

--- src/Posts.js
+++ src/Posts.js
@@ -1,19 +1,23 @@
+
+import React from 'react'
 export default function Posts() {
 
-  const posts = [{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/praia.jpeg', texto: 'Jonas Dias' }, { perfil: 'assets/img/eu.jpg', imagem: 'assets/img/bb.jpg', texto: 'Jonas Dias' },{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/anjo.jpg', texto: 'Jonas Dias' },{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/balerion.jpg', texto: 'Jonas Dias' }]
+  const posts = [{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/praia.jpeg', texto: 'Jonas Dias' , likes: 985}, { perfil: 'assets/img/eu.jpg', imagem: 'assets/img/bb.jpg', texto: 'Jonas Dias' , likes: 911},{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/anjo.jpg', texto: 'Jonas Dias' , likes: 1085},{ perfil: 'assets/img/eu.jpg', imagem: 'assets/img/balerion.jpg', texto: 'Jonas Dias' , likes: 895}]
 
 
 
   return (
         <div class='posts'>
-          {posts.map(p => <Post perfil={p.perfil} texto={p.texto} imagem={p.imagem}/>)}
+          {posts.map(p => <Post perfil={p.perfil} texto={p.texto} imagem={p.imagem} likes={p.likes}/>)}
         </div>
     )
 }
 
 
 function Post(props) {
-
+  const[salvar, setSalvar] = React.useState('bookmark-outline')
+  const[like, setLike] = React.useState('heart-outline')
+  const[curtidas, setLikes] = React.useState(props.likes)
   return (
     <div class="post">
       <div class="topo">
@@ -33,19 +37,29 @@ function Post(props) {
       <div class="fundo">
         <div class="acoes">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name={like} onClick ={() => {
+              if (like==='heart-outline'){
+                setLike('heart')
+                setLikes(curtidas+1)
+              }
+
+              else{
+                setLike('heart-outline')
+                setLikes(curtidas-1)
+              }
+            }}></ion-icon>
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
           <div>
-            <ion-icon name="bookmark-outline"></ion-icon>
+            <ion-icon onClick ={() => salvar==='bookmark-outline' ? setSalvar('bookmark') : setSalvar('bookmark-outline')} name={salvar}></ion-icon>
           </div>
         </div>
 
         <div class="curtidas">
           <img src="assets/img/mozao.jpeg" />
           <div class="texto">
-            Curtido por <strong>Mozão</strong> e <strong>outras 101.523 pessoas</strong>
+            Curtido por <strong>Mozão</strong> e outras <strong> {curtidas} </strong> pessoas
           </div>
         </div>
       </div>
