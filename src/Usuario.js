34f102a1170eafdd750d4634ba0fe580c90b755a
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-11-18 21:03:33 -0300

    Nome e imagem de usuário dinâmicos

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,21 +1,23 @@
+import React from 'react'
+
+
 export default function Usuario(props){
-
-
+  console.log(props.nome)
+  const [usuario, setPropsUsuario]= React.useState(props.nome)
+  const [url, setPropsUrl]= React.useState(props.imagem)
+  
 
     return(
         <div class="usuario">
-          <img src={props.imagem} />
+          <img onClick={() => url = setPropsUrl(prompt('Cole a nova url da imagem'))} src={url} />
           <div class="texto">
             <strong>{props.usuario}</strong>
             <span>
-              {props.nome}
-              <ion-icon  name="pencil"></ion-icon>
+              {usuario}
+              <ion-icon onClick={() => setPropsUsuario(prompt('Digite o novo nome de usuário'))} name="pencil"></ion-icon>
             </span>
           </div>
         </div>
     )
 }
 
-function editar(){
-  alert('funcionando')
-}
